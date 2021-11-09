01393570ac5b75fb71137d95b0a4b1e43de42404
SivaNagendra-sn <sivanagendra.paruchuri@gmail.com>
2021-11-09 08:56:01 +0530

    stage 1 utilities added

--- src/stage_1_prep_data.py
+++ src/stage_1_prep_data.py
@@ -3,9 +3,11 @@ import os
 import shutil
 from tqdm import tqdm
 import logging
-from utils.common import read_yaml
+from utils.common import read_yaml, create_directories
+import random
 
-STAGE = "Data Preparation"
+
+STAGE = "Data Preparation"  
 
 logging.basicConfig(
     filename=os.path.join("logs", 'running_logs.log'), 
@@ -15,9 +17,23 @@ logging.basicConfig(
     )
 
 def prep_data(config_path, params_path):
+    # Converting XML data to tsv
     config = read_yaml(config_path)
     params = read_yaml(params_path)
     print("red the data")
+
+    source_data = config["source_data"]
+
+    input_data = os.path.join(source_data["data_dir"],source_data["data_file"])
+
+    split_data = params["prepare"]["split"]
+    seed = params["prepare"]["seed"]
+    random.seed(seed)
+
+    artifacts = config["artifacts"]
+    prepared_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"],artifacts["PREPARED_DATA_DIR"])
+
+    create_directories([prepared_data_dir_path])
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
